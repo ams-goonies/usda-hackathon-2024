205c37246ec041c387600676e1a0cff867d13a39
aishwarya-sreenivasan <88390360+aishwarya-sreenivasan@users.noreply.github.com>
2024-07-01 08:57:02 -0500

    sales by states by all commodities (individually)

---
+++ data_prep/state data.R
@@ -0,0 +1,29 @@
+akey <- "91110E21-FB5B-3FDA-890D-8E84C1251469"
+
+#Get state level sales 2022
+
+state_sales <- tidyUSDA::getQuickstat(
+  sector = 'CROPS',
+  #group = c('FIELD CROPS', 'FRUIT & TREE NUTS', 'VEGETABLES'),
+  commodity='CROP TOTALS',
+  category='SALES',
+  #domain='SALES & DISTRIBUTION',
+  #county=NULL,
+  key = akey,
+  program = 'CENSUS',
+  #data_item = 'CROP TOTALS - SALES, MEASURED in $',
+  geographic_level = 'STATE',
+  year = '2022',
+  #state = 'ALABAMA',
+  geometry = FALSE,
+  lower48 = FALSE,
+  weighted_by_area = FALSE) %>%
+  filter(grepl("SALES, MEASURED IN", short_desc))
+
+cleandata <- select(state_sales, "Value", "state_alpha")
+cleandata <- na.omit(cleandata)
+total_sales <- cleandata %>% group_by(state_alpha)  %>%
+  summarize(total_sales = sum(Value))
+
+View(total_sales)
+
