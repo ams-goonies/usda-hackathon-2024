73a94ed18ea8a83df8ffc655466ef86587da6e78
aishwarya-sreenivasan <88390360+aishwarya-sreenivasan@users.noreply.github.com>
2024-07-05 12:12:54 -0500

    state and demographics plot

---
+++ scripts for app/hackathon plots.R
@@ -0,0 +1,46 @@
+library(dplyr)
+library(reactable)
+
+demdata <- state_demographics %>%
+  select(state_name, short_desc, farm_size, Value_2017, Value_2022, change_pct)
+
+demdata$change_pct <- as.numeric(demdata$change_pct)
+
+format_color <- function(value) {
+  if (is.na(value)) return(NULL)
+  
+  if (value > 0) {
+    color <- "rgba(0, 255, 0, 0.2)" 
+  } else if (value < 0) {
+    color <- "rgba(255, 0, 0, 0.2)"
+  } else {
+    color <- "transparent"
+  }
+  
+  list(backgroundColor = color)
+}
+
+reactable(demdata, 
+          columns = list(
+            state_name = colDef(name = "State"),
+            short_desc = colDef(name = "Demographic"),
+            farm_size = colDef(name = "Farm Size"),
+            Value_2017 = colDef(
+              name = "2017 Sales",
+              format = colFormat(prefix = "$", separators = TRUE, digits = 0)
+            ),
+            Value_2022 = colDef(
+              name = "2022 Sales",
+              format = colFormat(prefix = "$", separators = TRUE, digits = 0)
+            ),
+            change_pct = colDef(
+              name = "Change in Sales",
+              cell = function(value) {
+                if (is.na(value)) return(NA)
+                sprintf("%.2f%%", value)
+              },
+              style = function(value) format_color(value),
+              align = "right"
+            )
+          )
+)
